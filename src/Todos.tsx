ec651b893cdc685be42292aa413d14e7d9fd9682
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2020-12-17 00:09:03 +0530

    style `Todos` comp

--- src/Todos.tsx
+++ src/Todos.tsx
@@ -2,22 +2,32 @@ import React, { useEffect, useState } from 'react';
 
 import dbx from './dbx';
 
+import { Box, Center, Checkbox, Spinner, VStack } from '@chakra-ui/react';
+
 const todoFilePath = '/Apps/Simpletask/todo.txt';
 
 export const Todos = () => {
-  const [todos, setTodos] = useState(['']);
+  const [todos, setTodos] = useState<Array<string>>([]);
 
   useEffect(() => {
     dbx.openFile(todoFilePath).then((file) => {
-      setTodos(file.contents.split('\n'));
+      setTodos(file.contents.split('\n').filter((todo) => !!todo));
     });
   }, []);
 
   return (
-    <ul>
-      {todos.map((todo, id) => (
-        <li key={id}>{todo}</li>
-      ))}
-    </ul>
+    <Center>
+      {!todos.length ? (
+        <Spinner />
+      ) : (
+        <VStack align="stretch" width={{ lg: '650px' }} margin="3">
+          {todos.map((todo, id) => (
+            <Box padding="5" shadow="base" key={id}>
+              <Checkbox size="lg">{todo}</Checkbox>
+            </Box>
+          ))}
+        </VStack>
+      )}
+    </Center>
   );
 };
