6beeed29625ed5d304a8803e122c59442d94acbb
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2020-12-17 21:22:02 +0530

    add sample 'todo.txt' file for testing

---
+++ src/sample-todotxt.ts
@@ -0,0 +1,19 @@
+export const sampleTodotxtFile = {
+  contents: `
+(A) 2020-12-14 Complete unit 1.4.3-4 +LSKCourse due:2020-12-14
+(A) 2020-12-14 Report to AK +PhD @Phone due:2020-12-14
+(B) 2020-12-09 call Gururaja reg. RSS courses @Phone due:2020-12-14
+(B) 2020-12-14 Contact Vishvas Vasuki reg. other Sanskrit Spellchecker implementations @Comp/Mail due:2020-12-14
+x 2020-12-16 Recharge my phone @Phone due:2020-12-14 pri:B
+2019-01-09 Build custom indic transliteration system without caps and symbols (and covers all indic characters). Purpose: writing filenames. +vtranslit @Comp/Coding
+2019-03-08 Making astro calc software with mother @Comp/Coding
+2019-03-08 Manage computer data - Mac, Laptop, HD @Comp
+2019-03-08 Read Mani Mantra Aushadha synopsis @Comp
+2019-03-08 complete Python Game @Comp/Coding
+2019-04-08 Create a Linux from Scratch @Comp/Coding
+2019-04-17 Basic Hinduism for the Supreme Court @Comp/Mail
+2020-12-09 check amarakosha course page for mother @Comp due:2020-12-14
+x 2020-12-16 2020-12-14 Ask Lakshminarayan to send Research Methodology book @Phone due:2020-12-14
+2020-12-14 Clean mirror bero due:2020-12-15
+`,
+};
