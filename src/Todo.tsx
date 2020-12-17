ecc1d892fe2860cd2028a108ae790caff0ac4de6
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2020-12-17 21:27:10 +0530

    extract todo content to a separate `Todo` comp

---
+++ src/Todo.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import {
+  Box,
+  Checkbox,
+  HStack,
+  Tag,
+  TagLabel,
+  TagLeftIcon,
+  VStack,
+} from '@chakra-ui/react';
+import { AddIcon, AtSignIcon } from '@chakra-ui/icons';
+
+type Props = {
+  text: string;
+  contexts: string[];
+  projects: string[];
+};
+
+export const Todo: React.FC<Props> = ({ text, contexts, projects }) => {
+  const hasContexts = !!contexts.length;
+  const hasProjects = !!projects.length;
+
+  return (
+    <Box padding="5" shadow="base" bg="white">
+      <HStack spacing="3">
+        <Checkbox size="lg"></Checkbox>
+        <VStack align="start">
+          <Box>{text}</Box>
+          <HStack>
+            {hasContexts &&
+              contexts.map((context) => (
+                <Tag colorScheme="blue">
+                  <TagLeftIcon as={AtSignIcon} boxSize="12px"></TagLeftIcon>
+                  <TagLabel>{context.slice(1)}</TagLabel>
+                </Tag>
+              ))}
+            {hasProjects &&
+              projects.map((project) => (
+                <Tag colorScheme="orange">
+                  <TagLeftIcon as={AddIcon} boxSize="12px"></TagLeftIcon>
+                  <TagLabel>{project.slice(1)}</TagLabel>
+                </Tag>
+              ))}
+          </HStack>
+        </VStack>
+      </HStack>
+    </Box>
+  );
+};
--- src/Todos.tsx
+++ src/Todos.tsx
@@ -4,7 +4,11 @@ import dbx from './dbx';
 
 import { TodoTxt } from './todotxt';
 
-import { Box, Center, Checkbox, Spinner, VStack } from '@chakra-ui/react';
+import { Spinner, VStack } from '@chakra-ui/react';
+
+import { Todo } from './Todo';
+
+// import { sampleTodotxtFile as file } from './sample-todotxt';
 
 const todoFilePath = '/Apps/Simpletask/todo.txt';
 
@@ -12,28 +16,27 @@ export const Todos = () => {
   const [todos, setTodos] = useState<Array<any>>([]);
 
   useEffect(() => {
-    dbx.openFile(todoFilePath).then((file) => {
+    (async () => {
+      const file = await dbx.openFile(todoFilePath);
       const todos = TodoTxt.parseFile(file.contents);
 
       setTodos(todos.items());
-    });
+    })();
   }, []);
 
-  return (
-    <Center>
-      {!todos.length ? (
-        <Spinner />
-      ) : (
-        <VStack align="stretch" width={{ lg: '650px' }} margin="3">
-          {todos.map((todo, id) => (
-            <Box padding="5" shadow="base" key={id}>
-              <Checkbox size="lg" spacing="3">
-                {todo.textTokens().join(' ')}
-              </Checkbox>
-            </Box>
-          ))}
-        </VStack>
-      )}
-    </Center>
+  return !todos.length ? (
+    <Spinner />
+  ) : (
+    <VStack align="stretch" width={{ lg: '650px' }} margin="3">
+      {todos.map((todo, id) => {
+        const text = todo.textTokens().join(' ');
+        const contexts = todo.contexts();
+        const projects = todo.projects();
+
+        return (
+          <Todo text={text} contexts={contexts} projects={projects} key={id} />
+        );
+      })}
+    </VStack>
   );
 };
