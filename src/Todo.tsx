44417047985358c47cd2d71d54583b2fd3265a67
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2020-12-19 20:50:49 +0530

    avoid extra being added inside todo box
    when there are no projects or contexts

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -27,22 +27,24 @@ export const Todo: React.FC<Props> = ({ text, contexts, projects }) => {
         <Checkbox size="lg"></Checkbox>
         <VStack align="start">
           <Box>{text}</Box>
-          <HStack>
-            {hasContexts &&
-              contexts.map((context) => (
-                <Tag colorScheme="blue">
-                  <TagLeftIcon as={AtSignIcon} boxSize="12px"></TagLeftIcon>
-                  <TagLabel>{context.slice(1)}</TagLabel>
-                </Tag>
-              ))}
-            {hasProjects &&
-              projects.map((project) => (
-                <Tag colorScheme="orange">
-                  <TagLeftIcon as={AddIcon} boxSize="12px"></TagLeftIcon>
-                  <TagLabel>{project.slice(1)}</TagLabel>
-                </Tag>
-              ))}
-          </HStack>
+          {(hasContexts || hasProjects) && (
+            <HStack>
+              {hasContexts &&
+                contexts.map((context) => (
+                  <Tag colorScheme="blue">
+                    <TagLeftIcon as={AtSignIcon} boxSize="12px"></TagLeftIcon>
+                    <TagLabel>{context.slice(1)}</TagLabel>
+                  </Tag>
+                ))}
+              {hasProjects &&
+                projects.map((project) => (
+                  <Tag colorScheme="orange">
+                    <TagLeftIcon as={AddIcon} boxSize="12px"></TagLeftIcon>
+                    <TagLabel>{project.slice(1)}</TagLabel>
+                  </Tag>
+                ))}
+            </HStack>
+          )}
         </VStack>
       </HStack>
     </Box>
