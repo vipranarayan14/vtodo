17471920be23501ebd010ea2ea64fbf912b1569f
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2020-12-22 00:53:09 +0530

    store todos obj instead of todos.items

--- src/Todos.tsx
+++ src/Todos.tsx
@@ -1,39 +1,54 @@
 import React, { useEffect, useState } from 'react';
 
-import dbx from './dbx';
+import dbxClient from './dbx';
 
 import { TodoTxt } from './todotxt';
 
-import { Spinner, VStack } from '@chakra-ui/react';
+import { Box, Spinner, VStack } from '@chakra-ui/react';
 
 import { Todo } from './Todo';
 
 // import { sampleTodotxtFile as file } from './sample-todotxt';
 
 type Props = {
+  dbxAccessToken: string;
   setTasksCount: (count: number) => void;
 };
 
 const todoFilePath = '/Apps/Simpletask/todo.txt';
 
-export const Todos: React.FC<Props> = ({ setTasksCount }) => {
-  const [todos, setTodos] = useState<Array<any>>([]);
+export const Todos: React.FC<Props> = ({
+  setTasksCount,
+  dbxAccessToken: accessToken,
+}) => {
+  const [todos, setTodos] = useState<any>();
+  const [showError, setShowError] = useState<boolean>(false);
 
   useEffect(() => {
     (async () => {
-      const file = await dbx.openFile(todoFilePath);
+      const [file, error] = await dbxClient.openFile(todoFilePath, accessToken);
+
+      if (error) {
+        setShowError(true);
+        return;
+      }
+
       const todos = TodoTxt.parseFile(file.contents);
 
-      setTodos(todos.items());
+      setTodos(todos);
       setTasksCount(todos.items().length);
     })();
-  }, [setTasksCount]);
+  }, [setTasksCount, accessToken]);
 
-  return !todos.length ? (
+  if (showError) {
+    return <Box>Unable to get 'todo.txt' file.</Box>;
+  }
+
+  return !todos?.items().length ? (
     <Spinner />
   ) : (
     <VStack align="stretch" width={{ lg: '650px' }} margin="2">
-      {todos.map((todo) => (
+      {todos.items().map((todo: any) => (
         <Todo todo={todo} key={todo.id()} />
       ))}
     </VStack>
