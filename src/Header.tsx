d2db3107d556b55a1dcb2f0c932e49cb9fd96acb
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2020-12-22 13:58:45 +0530

    make `Header` a generic comp
    so that it can be reused in various pages.

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 import { Box, Heading, Text } from '@chakra-ui/react';
 
 type Props = {
-  tasksCount: number;
+  heading: string;
+  subHeading?: string;
 };
 
-export const Header: React.FC<Props> = ({ tasksCount }) => (
+export const Header: React.FC<Props> = ({ heading, subHeading }) => (
   <Box
     as="header"
     bg="white"
@@ -18,11 +19,13 @@ export const Header: React.FC<Props> = ({ tasksCount }) => (
   >
     <Box width={{ lg: '650px' }} p="3" mx="auto">
       <Heading as="h1" textStyle="h1" ml="1">
-        All Tasks
+        {heading}
       </Heading>
-      <Text fontSize="md" ml="2">
-        {tasksCount} tasks
-      </Text>
+      {subHeading && (
+        <Text fontSize="md" ml="2">
+          {subHeading}
+        </Text>
+      )}
     </Box>
   </Box>
 );
