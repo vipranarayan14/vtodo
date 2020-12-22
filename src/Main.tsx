9701cef2bf3214061ec36c583effd1c43536b3e4
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2020-12-22 13:59:59 +0530

    create a reusable `Main` comp

---
+++ src/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Box } from '@chakra-ui/react';
+
+type Props = {
+  children: any;
+};
+
+export const Main: React.FC<Props> = ({ children }) => (
+  <Box width={{ lg: '650px' }} mx="auto">
+    {children}
+  </Box>
+);
