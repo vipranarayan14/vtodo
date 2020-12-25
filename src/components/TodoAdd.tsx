75db368b767d1e3ea944b41693cb15b53c6a9a00
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2020-12-25 17:19:02 +0530

    feat: add support adding new todos - part 2
    Large screen view
    
    Extract contents of `DrawerBody` and `DrawerFooter` to separate
    components and reuse it different containers based on screen size:
    1) Small screen view: Drawer
    2) Large screen view: inline prepending `Todos` comp

--- src/components/TodoAdd.tsx
+++ src/components/TodoAdd.tsx
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
 
 import {
+  Box,
+  Button,
   Drawer,
   DrawerBody,
   DrawerContent,
   DrawerFooter,
   DrawerOverlay,
+  Flex,
   HStack,
   IconButton,
+  Input,
   Spacer,
   Textarea,
+  useBreakpointValue,
   useDisclosure,
 } from '@chakra-ui/react';
 
@@ -28,15 +33,90 @@ type Props = {
   addTodo: (todoString: string) => void;
 };
 
+type BodyProps = {
+  todoString: string;
+  setTodoString: any;
+  handleSendClick: any;
+};
+
+const Body: React.FC<BodyProps> = ({
+  todoString,
+  setTodoString,
+  handleSendClick,
+}) => (
+  <>
+    <HStack spacing="1" alignItems={{ base: 'center', lg: 'start' }}>
+      <Textarea
+        placeholder="Add a task..."
+        defaultValue={todoString}
+        onChange={(e) => setTodoString(e.target.value)}
+        autoFocus
+        resize="none"
+        fontWeight="500"
+        border="none"
+        lineHeight="inherit" // = 24px; to match with the placeholder input
+        focusBorderColor="none"
+        _placeholder={{ opacity: 1 }}
+      />
+      <IconButton
+        aria-label="Add new todo"
+        icon={<AiOutlineSend />}
+        size="lg"
+        height="2.5rem"
+        variant="transparent"
+        onClick={handleSendClick}
+      />
+    </HStack>
+  </>
+);
+
+const Footer: React.FC = () => (
+  <>
+    <IconButton
+      aria-label="Add priority"
+      icon={<AiOutlineFlag />}
+      size="lg"
+      variant="transparent"
+    />
+    {/* <Spacer /> */}
+    <IconButton
+      aria-label="Add project"
+      icon={<AiOutlineTag />}
+      size="lg"
+      variant="transparent"
+    />
+    {/* <Spacer /> */}
+    <IconButton
+      aria-label="Add context"
+      icon={<FiAtSign />}
+      size="lg"
+      variant="transparent"
+    />
+    {/* <Spacer /> */}
+    <IconButton
+      aria-label="Add due date"
+      icon={<AiOutlineClockCircle />}
+      size="lg"
+      variant="transparent"
+    />
+    {/* <Spacer /> */}
+    <IconButton
+      aria-label="Add threshold date"
+      icon={<AiOutlineCalendar />}
+      size="lg"
+      variant="transparent"
+    />
+  </>
+);
+
 export const TodoAdd: React.FC<Props> = ({ addTodo }) => {
-  const todoStringInitialState = ' +Inbox';
+  const todoStringInitialState = '';
 
   const [todoString, setTodoString] = useState(todoStringInitialState);
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const initialFocus = React.useRef(null);
-  const finalFocus = React.useRef(null);
+  const isScreenSizeLarge = useBreakpointValue({ lg: true }); // breakpoint: lg
 
   const handleSendClick = () => {
     addTodo(todoString);
@@ -44,10 +124,54 @@ export const TodoAdd: React.FC<Props> = ({ addTodo }) => {
     setTodoString(todoStringInitialState);
   };
 
+  if (isScreenSizeLarge) {
+    return (
+      <Box bg="white" borderRadius="10px" p="1" shadow="md" mt="2" mb="5">
+        {!isOpen ? (
+          <HStack spacing="1">
+            <Input
+              onFocus={onOpen}
+              placeholder="Add a task..."
+              bg="white"
+              fontWeight="500"
+              border="none"
+              focusBorderColor="none"
+              _placeholder={{ opacity: 1 }}
+            />
+            <IconButton
+              aria-label="Add new todo"
+              icon={<AiOutlineSend />}
+              size="lg"
+              height="2.5rem"
+              variant="transparent"
+              isDisabled
+            />
+          </HStack>
+        ) : (
+          <>
+            <Box>
+              <Body
+                todoString={todoString}
+                setTodoString={setTodoString}
+                handleSendClick={handleSendClick}
+              />
+            </Box>
+            <Flex borderTopWidth="1px" mt="1" py="1" justifyContent="start">
+              <Footer />
+              <Spacer />
+              <Button onClick={onClose} size="lg">
+                Close
+              </Button>
+            </Flex>
+          </>
+        )}
+      </Box>
+    );
+  }
+
   return (
     <>
       <IconButton
-        ref={finalFocus}
         aria-label="Add todo"
         icon={<AddIcon />}
         onClick={onOpen}
@@ -59,76 +183,23 @@ export const TodoAdd: React.FC<Props> = ({ addTodo }) => {
         right="20px"
         isRound
       ></IconButton>
-      <Drawer
-        isOpen={isOpen}
-        onClose={onClose}
-        initialFocusRef={initialFocus}
-        finalFocusRef={finalFocus}
-        placement="bottom"
-      >
+
+      <Drawer isOpen={isOpen} onClose={onClose} placement="bottom">
         <DrawerOverlay />
         <DrawerContent borderTopRadius="10px">
           <DrawerBody p="1">
-            <HStack spacing="1">
-              <Textarea
-                ref={initialFocus}
-                defaultValue={todoString}
-                onChange={(e) => setTodoString(e.target.value)}
-                resize="none"
-                fontWeight="500"
-                px="8px"
-                placeholder="Add a task..."
-              />
-              <IconButton
-                aria-label="Add new todo"
-                icon={<AiOutlineSend />}
-                size="lg"
-                variant="transparent"
-                onClick={handleSendClick}
-              />
-            </HStack>
+            <Body
+              todoString={todoString}
+              setTodoString={setTodoString}
+              handleSendClick={handleSendClick}
+            />
           </DrawerBody>
-          <DrawerFooter borderTopWidth="1px" py="1" justifyContent="start">
-            <IconButton
-              aria-label="Add priority"
-              icon={<AiOutlineFlag />}
-              size="lg"
-              variant="transparent"
-            />
-
-            <Spacer />
-
-            <IconButton
-              aria-label="Add project"
-              icon={<AiOutlineTag />}
-              size="lg"
-              variant="transparent"
-            />
-
-            <Spacer />
-
-            <IconButton
-              aria-label="Add context"
-              icon={<FiAtSign />}
-              variant="transparent"
-            />
-            <Spacer />
-
-            <IconButton
-              aria-label="Add due date"
-              icon={<AiOutlineClockCircle />}
-              size="lg"
-              variant="transparent"
-            />
-
-            <Spacer />
-
-            <IconButton
-              aria-label="Add threshold date"
-              icon={<AiOutlineCalendar />}
-              size="lg"
-              variant="transparent"
-            />
+          <DrawerFooter
+            borderTopWidth="1px"
+            py="1"
+            justifyContent="space-between"
+          >
+            <Footer />
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
